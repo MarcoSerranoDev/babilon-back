0cc5db28867cbb7a1ccdd442426b3c730f4565eb
Emmanuel Martinez <emmanuel.martinez@hermes-music.com.mx>
2025-08-07 13:54:23 -0600

    configuracion de las cors

--- src/app.js
+++ src/app.js
@@ -1,34 +1,36 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
-import { createRoles } from "./libs/initialSetup";
-import productsRoutes from "./routes/products.routes";
-import authRoutes from "./routes/auth.routes";
-import userRoutes from "./routes/user.routes";
-import artistRoutes from "./routes/artist.routes";
-import guitarRoutes from "./routes/guitar.routes";
-import eventRoutes from "./routes/event.routes";
-import serieRoutes from "./routes/serie.routes";
+import { createRoles } from './libs/initialSetup';
+import productsRoutes from './routes/products.routes';
+import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/user.routes';
+import artistRoutes from './routes/artist.routes';
+import guitarRoutes from './routes/guitar.routes';
+import eventRoutes from './routes/event.routes';
+import serieRoutes from './routes/serie.routes';
 
 const app = express();
 createRoles();
 
-app.set("port", process.env.PORT || 4000);
+app.set('port', process.env.PORT || 4000);
 
 //Meddlewares
-const corsOptions = {};
+const corsOptions = {
+  origin: 'https://www.babilonguitars.com',
+};
 app.use(cors(corsOptions));
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/babilon/v1/products", productsRoutes);
-app.use("/api/babilon/v1/auth", authRoutes);
-app.use("/api/babilon/v1/users", userRoutes);
-app.use("/api/babilon/v1/artist", artistRoutes);
-app.use("/api/babilon/v1/guitar", guitarRoutes);
-app.use("/api/babilon/v1/events", eventRoutes);
-app.use("/api/babilon/v1/serie", serieRoutes);
+app.use('/api/babilon/v1/products', productsRoutes);
+app.use('/api/babilon/v1/auth', authRoutes);
+app.use('/api/babilon/v1/users', userRoutes);
+app.use('/api/babilon/v1/artist', artistRoutes);
+app.use('/api/babilon/v1/guitar', guitarRoutes);
+app.use('/api/babilon/v1/events', eventRoutes);
+app.use('/api/babilon/v1/serie', serieRoutes);
 
 export default app;
